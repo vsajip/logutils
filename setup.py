188b331205ab6a0e5a8a9039d4b3a984f5234467
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2019-06-07 21:42:06 +0100

    Removed print statement.

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ def missing_files():
 
         FILES = ('cat.exe', 'echo.exe', 'tee.exe', 'false.exe', 'true.exe',
                  'sleep.exe', 'touch.exe')
-        print('TESTING_ON_AV: %s' % os.environ.get('TESTING_ON_AV'))
         if 'TESTING_ON_AV' in os.environ:
             FILES = ('libiconv2.dll', 'libintl3.dll') + FILES
 
